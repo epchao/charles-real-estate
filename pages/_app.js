69927aa93012e15e598677af2b1d61e627f78261
Eugene Chao <eugenechao00@gmail.com>
2021-10-22 15:04:05 -0700

    Added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import App from 'next/app'
+import Head from 'next/head'
 import '../styles/globals.css'
 import { createContext } from 'react'
 import { fetchAPI } from '../lib/strapiurl'
@@ -10,6 +11,14 @@ const { global } = pageProps
 
   return (
     <>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+        <meta name="msapplication-TileColor" content="#da532c"/>
+        <meta name="theme-color" content="#ffffff"/>
+      </Head>
       <GlobalContext.Provider value={global}>
         <Component {...pageProps} />
       </GlobalContext.Provider>
